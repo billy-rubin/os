9beb2cf8bcbaca0888fbce614206a1963ef9887a
billy-rubin <rumit211@gmail.com>
2025-10-09 09:17:43 +0700

    1-1, 1-2

---
+++ threads/1-2.c
@@ -0,0 +1,45 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <pthread.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+void* mythread(void* arg) {
+    /*
+    printf("mythread [%d %d %d %ld]: Hello from mythread!\n", getpid(), getppid(), gettid(), pthread_self());
+    return NULL;
+    */
+   /*
+   int *result = malloc(sizeof(int));
+    *result = 42;
+    return result;
+   */
+    return "Hello world\n";
+
+}
+
+int main(void) {
+    pthread_t tid;
+    if (pthread_create(&tid, NULL, mythread, NULL) != 0) {
+        perror("pthread_create");
+        return 1;
+    }
+    /*
+    pthread_join(tid, NULL);
+    printf("main thread waited for thread to finish\n");
+    */
+   
+   /*
+   int* res;
+   */
+    
+    char *res;
+    pthread_join(tid, (void**)&res);
+    
+    printf("%d\n", *res);
+
+    return 0;
+}
--- threads/thread.c
+++ threads/thread.c
@@ -5,23 +5,49 @@
 #include <errno.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <stdlib.h>
+
+int global = 1;
 
 void *mythread(void *arg) {
-	printf("mythread [%d %d %d]: Hello from mythread!\n", getpid(), getppid(), gettid());
+	int local = 5;
+    static int localStatic = 10;
+    const int localConst = 7;
+
+	printf("mythread [%d %d %d %ld]: Hello from mythread!\n", getpid(), getppid(), gettid(), pthread_self());
+	printf("addresses - local: %p static: %p const: %p global: %p\n", &local, &localStatic, &localConst, &global);
+	printf("values - local: %d static: %d const: %d global: %d\n", local, localStatic, localConst, global);
+
+	local += 1;
+    global += 1;
+	printf("local value - %d, global value - %d", local, global);
+	sleep(5);
 	return NULL;
 }
 
 int main() {
-	pthread_t tid;
+    pthread_t threads[5];	
 	int err;
 
 	printf("main [%d %d %d]: Hello from main!\n", getpid(), getppid(), gettid());
 
-	err = pthread_create(&tid, NULL, mythread, NULL);
-	if (err) {
-	    printf("main: pthread_create() failed: %s\n", strerror(err));
-		return -1;
-	}
+	for (int i = 0; i < 5; i++) {
+
+		err = pthread_create(&threads[i], NULL, mythread, NULL);
+		printf("main [%ld]\n", threads[i]);
+		if (err) {
+		    printf("main: pthread_create() failed: %s\n", strerror(err));
+			for (int j = 0; j < i; j++) {
+				pthread_join(threads[j], NULL);
+			}
+			return -1;
+		}
+		sleep(1000);
+    }
+
+	for (int i = 0; i < 5; i++) {
+        pthread_join(threads[i], NULL);
+    }
 
 	return 0;
 }
