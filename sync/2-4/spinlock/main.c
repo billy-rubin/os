2c756e941353cc37fcfdf4538754df3766650c92
billy-rubin <rumit211@gmail.com>
2025-12-24 22:44:13 +0700

    spinlock first implementation

--- sync/2-4/spinlock/main.c
+++ sync/2-4/spinlock/main.c
@@ -1,14 +1,15 @@
+#define _GNU_SOURCE 
 #include <stdio.h>
 #include <pthread.h>
 #include <time.h>
-#include "mutex.h"   
+#include "spinlock.h"   
 
 #define N_THREADS 4
 #define N_ITER    10000
 
-static long long counter_futex = 0;
+static long long counter_spin    = 0;
 
-static futex_mutex_t futex_mtx;
+static spinlock_t         spin_lock_my;
 
 static double now_sec(void) {
     struct timespec ts;
@@ -16,14 +17,13 @@ static double now_sec(void) {
     return ts.tv_sec + ts.tv_nsec * 1e-9;
 }
 
-
-void* worker_futex(void *arg) {
+void* worker_spin(void *arg) {
     (void)arg;
 
     for (int i = 0; i < N_ITER; ++i) {
-        fmutex_lock(&futex_mtx);
-        counter_futex++;
-        fmutex_unlock(&futex_mtx);
+        spin_lock(&spin_lock_my);
+        counter_spin++;
+        spin_unlock(&spin_lock_my);
     }
 
     return NULL;
@@ -32,13 +32,14 @@ void* worker_futex(void *arg) {
 int main(void) {
     pthread_t threads[N_THREADS];
 
-    fmutex_init(&futex_mtx);
-    printf("=== fmutex_t (my) ===\n");
+    spin_init(&spin_lock_my);
+
+    printf("=== spinlock_t (my) ===\n");
 
     double t0 = now_sec();
 
     for (int i = 0; i < N_THREADS; ++i)
-        pthread_create(&threads[i], NULL, worker_futex, NULL);
+        pthread_create(&threads[i], NULL, worker_spin, NULL);
 
     for (int i = 0; i < N_THREADS; ++i)
         pthread_join(threads[i], NULL);
@@ -46,8 +47,7 @@ int main(void) {
     double t1 = now_sec();
 
     printf("Expected: %lld\n", (long long)N_THREADS * N_ITER);
-    printf("Actual:   %lld\n", counter_futex);
+    printf("Actual:   %lld\n", counter_spin);
     printf("Time:     %.6f s\n\n", t1 - t0);
-
     return 0;
 }
